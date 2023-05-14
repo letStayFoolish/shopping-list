51387b105d885dda2d476d4f941ae3b30695cfb8
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-05-14 13:54:14 +0700

    Add Items To Local Storage

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function createIcon(classes) {
   return icon;
 }
 
-function addItem(event) {
+function onAddItemSubmit(event) {
   event.preventDefault();
   const newItem = itemInput.value;
 
@@ -29,19 +29,42 @@ function addItem(event) {
     alert('Please add the item.');
     return;
   }
+  // Create item DOM element:
+  addItemToDOM(newItem);
+  // Add item to local storage:
+  addItemToLocalStorage(newItem);
 
+  checkUI();
+  itemInput.value = '';
+}
+
+function addItemToDOM(item) {
   // Create list item:
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
   // Add items to DOM:
   itemList.appendChild(li);
-
-  checkUI();
-  itemInput.value = '';
 }
+
+function addItemToLocalStorage(item) {
+  let itemsFromStorage;
+
+  if (localStorage.getItem('items') === null) {
+    itemsFromStorage = [];
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+  }
+
+  // Add new item to array:
+  itemsFromStorage.push(item);
+
+  // Convert to a JSON string and set to local storage:
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage));
+}
+
 // Event Deligation
 function removeItem(event) {
   console.log(event.target.parentElement);
@@ -89,7 +112,7 @@ function checkUI() {
 }
 
 // Event Listeners
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
 itemList.addEventListener('click', removeItem);
 clearButton.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems);
